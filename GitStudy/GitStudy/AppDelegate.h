7a30bf3529f907a649a645ceecefede4a8602bf3
shandian <shandian@juanpi.com>
2016-12-02 09:49:13 +0800

    Chore: 初始化项目

---
+++ GitStudy/GitStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitStudy
+//
+//  Created by Liuguiliang on 2016/12/2.
+//  Copyright © 2016年 HuiDragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitStudy/GitStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitStudy
+//
+//  Created by Liuguiliang on 2016/12/2.
+//  Copyright © 2016年 HuiDragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
