d69995bce7c39c4537f18e9bd5435f025b0d2597
shandian <shandian@juanpi.com>
2016-12-02 10:27:13 +0800

    Chore: 添加 静态单元格 和 UIStackView

---
+++ GitStudy/GitStudy/LGLViewController.m
@@ -0,0 +1,17 @@
+//
+//  ViewController.m
+//  GitStudy
+//
+//  Created by Liuguiliang on 2016/12/2.
+//  Copyright © 2016年 HuiDragon. All rights reserved.
+//
+
+#import "LGViewController.h"
+
+@interface LGViewController () 
+
+@end
+
+@implementation LGViewController
+ 
+@end
